8f8054545214eb30530a30ae1a2541a6bada8e4f
betty <be22250@gmail.com>
2020-10-30 11:48:03 +0800

    first_MVC_Commit

---
+++ UI_2020_1029/Controllers/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UI_2020_1029.Controllers
+{
+    public class LoginController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+      
+        public IActionResult PartView_Login(string sumbit)
+        {
+            if (sumbit == "sumbit")
+            {
+                //按鈕接值設
+                //回傳該頁主頁
+                return View("~/Views/Home/Index.cshtml");
+            }
+            else
+            {
+                //維持在Login 畫面
+                return View();
+            }
+           
+        }
+   
+    }
+}
---
+++ UI_2020_1029/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UI_2020_1029.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+
+        public 
+    }
+}
